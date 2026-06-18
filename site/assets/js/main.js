b53f63cfa46c2f05ee46f15784e07f8d7dac07b2
Rick Boyce <{ID}+{username}@users.noreply.github.com>
2026-06-18 19:58:11 +0100

    HIde events and weather sections until the JS init had completed in case of error or no script

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -2,10 +2,18 @@ import { setupEvents } from "./events.js";
 import { setupWeather } from "./weather.js";
 import { setupWebcam } from "./webcam.js";
 
+function revealJsDrivenSections() {
+    document.querySelectorAll("[data-js-driven]").forEach((section) => {
+        section.hidden = false;
+    });
+}
+
 function init() {
     setupWebcam();
+
     setupWeather();
     setupEvents();
+    revealJsDrivenSections();
 }
 
 if (document.readyState === "loading") {
